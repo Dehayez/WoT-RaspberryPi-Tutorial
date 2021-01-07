89997b07a0167c8566acea9f15cd6d9e17acf6a7
pgmgent-{jeroenvervaeck} <{jeroverv@student.arteveldehs.be}>
2021-01-07 17:58:51 +0100

    wip

--- client/src/App/components/index.js
+++ client/src/App/components/index.js
@@ -1,7 +1,9 @@
-export { default as GlTransmissionFormat } from "./3Dmodel";
+export { default as Arcade_Machine } from "./3Dmodel";
+export { default as Sparks } from "./Sparks";
 export { default as LightingStudio } from "./lighting/Studio";
 export { default as MainNavigation } from "./nav/MainNavigation";
 export { default as Problem } from "./notes/Problem";
 export { default as NextLeft } from "./next/NextLeft";
 export { default as NextRight } from "./next/NextRight";
+export { default as Blog } from "./blogs/Blog";
 
--- client/src/App/pages/extra/InfoPage.js
+++ client/src/App/pages/extra/InfoPage.js
@@ -9,7 +9,15 @@ import {  } from '../../components'
 const InfoPage = () => {
 	return (
 		<div className="info">
-			infopage
+			<div className="info__title">
+				<h1>Info</h1>
+				<p>
+					We willen jullie ook tonen wat er nog mogelijk is met jullie RaspberryPi.
+					De Mogelijkheden zijn eindeloos en je kan er zeer creatief mee omgaan.
+					Met deze voorbeelden kunnen we je al eens ontdekken wat je kan ontwikkelen.
+				</p>
+			</div>
+			<div className="info__blogs"></div>
 		</div>
 	);
 };
