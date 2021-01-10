26a87ad45bd002aac00a3dbb25d4bb3a5f1a007f
YentelDeHauwere <yentel.dh@hotmail.be>
2021-01-10 20:25:09 +0100

    responsive fixes

--- src/App/pages/extra/ContactPage.js
+++ src/App/pages/extra/ContactPage.js
@@ -1,17 +1,15 @@
-import { default as React, useEffect, useState } from 'react';
+import { default as React } from 'react';
 
 
 import './extra.scss';
 
 const ContactPage = () => {
-
-	const [ inputValue, setInputValue ] = useState('');
  
 	return (
 		<div className="contact">
 			<div className="contact__title">
 			<h1>Contact us</h1>
-				<p>
+				<p className="home__data-text">
 					Wanneer je problemen ondervindt bij het opzetten van je RaspberryPi of het RetroPi systeem,
 					kan je ons contacteren op dit mailadress:
 				</p>
